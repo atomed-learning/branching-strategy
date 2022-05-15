5ced404648408fccc1d42f664fb6e7bd63c0213c
Ivan Afonchenko <afon.himself@gmail.com>
2022-05-15 22:15:01 +0300

    EG-2 fix number module (#2)
    * EG-2 fix number module
    
    * EG-2 fix

--- modules/number.js
+++ modules/number.js
@@ -1,5 +1,5 @@
 module.exports = {
-  getRandomNumber(max) {
-    return Math.floor(Math.random() * max)
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min))
   }
 }
